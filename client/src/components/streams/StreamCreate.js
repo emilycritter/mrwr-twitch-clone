2829c94e8cd991a7eb2c63bb485d186aabffa764
emily c. ritter <emily.c.ritter@gmail.com>
2021-03-31 17:59:48 -0500

    Only show error messages if user have interacted with the input field

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -7,7 +7,7 @@ class StreamCreate extends React.Component {
       <div className="field">
         <label>{label}</label>
         <input {...input} />
-        <div>{meta.error}</div>
+        <div>{meta.touched ? meta.error : null}</div>
       </div>
     );
   }
