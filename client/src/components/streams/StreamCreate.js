1b570d0c17d65903844618d0865918491d8926eb
emily c. ritter <emily.c.ritter@gmail.com>
2021-03-31 17:44:44 -0500

    Refactors input field using es2015 syntax

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,13 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
   renderInput(formProps) {
-    return (
-      <input
-        className=""
-        value={formProps.input.value} 
-        onChange={formProps.input.onChange}
-        />
-    );
+    return <input {...formProps.input} />;
   }
 
   render() {
